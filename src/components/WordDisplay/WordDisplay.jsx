f694e9088937a6f07ee4c801bd63d70030d506a8
Espadv69 <aitorjob69@gmail.com>
2025-03-03 15:13:09 +0100

    Implement WordDisplay component to show guessed letters

--- src/components/WordDisplay/WordDisplay.jsx
+++ src/components/WordDisplay/WordDisplay.jsx
@@ -1,3 +1,15 @@
-const WordDisplay = () => {}
+import './WordDisplay.css'
+
+const WordDisplay = ({ word, guessedLetters }) => {
+  return (
+    <div className="word-display">
+      {word.split('').map((letter, index) => (
+        <span key={index} className="letter">
+          {guessedLetters.includes(letter) ? letter : '_'}
+        </span>
+      ))}
+    </div>
+  )
+}
 
 export default WordDisplay
