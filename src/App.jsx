9e7ad4aa35a6def4cbb4d7a11880d86b1df605ac
Espadv69 <aitorjob69@gmail.com>
2025-03-03 15:16:10 +0100

    Update guessed letters state in App component and pass to WordDisplay

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import WordDisplay from './components/WordDisplay/WordDisplay'
 const App = () => {
   // Simulate a word to guess
   const [word] = useState('REACT')
-  const [guessedLetters] = useState(['R', 'E'])
+  const [guessedLetters] = useState(['R', 'C'])
 
   return (
     <div className="app">
       <Header />
+      <WordDisplay word={word} guessedLetters={guessedLetters} />
     </div>
   )
 }
