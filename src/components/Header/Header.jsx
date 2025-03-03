044ed133b2bf32cc073eb6ec5b1f49ea8119cfb8
Espadv69 <aitorjob69@gmail.com>
2025-03-03 15:07:33 +0100

    Add Header component with title and instructions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,12 @@
-const Header = () => {}
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Hangman Game</h1>
+      <p>Guess the word before you run out of attempts!</p>
+    </header>
+  )
+}
 
 export default Header
