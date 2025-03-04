f7e1994968ac5be33d0f639359a49b0d0aef02d7
Espadv69 <aitorjob69@gmail.com>
2025-03-04 17:27:16 +0100

    Add letter click handler to update guessed letters in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,20 @@ const App = () => {
   const [word] = useState('REACT') // Word to guess
   const [guessedLetters, setGuessedLetters] = useState([]) // Store guessed letters
 
+  const handleLetterClick = (letter) => {
+    if (!guessedLetters.includes(letter)) {
+      setGuessedLetters([...guessedLetters, letter])
+    }
+  }
+
   return (
     <div className="app">
       <Header />
       <WordDisplay word={word} guessedLetters={guessedLetters} />
+      <KeyBoard
+        guessedLetters={guessedLetters}
+        onLetterClick={handleLetterClick}
+      />
     </div>
   )
 }
