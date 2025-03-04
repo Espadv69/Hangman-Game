5c8feaf6e9a6f21ad00e145ce703c5d49a340f33
Espadv69 <aitorjob69@gmail.com>
2025-03-04 18:30:25 +0100

    Implement RestartButton component with onRestart handler

--- src/components/RestartButton/RestartButton.jsx
+++ src/components/RestartButton/RestartButton.jsx
@@ -1,3 +1,9 @@
-const RestartButton = () => {}
+const RestartButton = ({ onRestart }) => {
+  return (
+    <button className="restart-button" onClick={onRestart}>
+      Restart Game
+    </button>
+  )
+}
 
 export default RestartButton
