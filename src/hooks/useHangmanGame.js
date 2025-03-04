3aeec444db6c9521509613fa4235b9fefdae2295
Espadv69 <aitorjob69@gmail.com>
2025-03-04 20:12:54 +0100

    Add maxAttempts and calculate incorrect letters in useHangmanGame hook

--- src/hooks/useHangmanGame.js
+++ src/hooks/useHangmanGame.js
@@ -4,4 +4,9 @@ import { getRandomWord } from '../utils/words'
 export const useHangmanGame = () => {
   const [word, setWord] = useState(getRandomWord())
   const [guessedLetters, setGuessedLetters] = useState([])
+  const maxAttempts = 6
+
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !word.includes(letter)
+  )
 }
