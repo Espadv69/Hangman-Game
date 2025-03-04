84496e3b511245eae66e7a25a2aaada0a88cf8d1
Espadv69 <aitorjob69@gmail.com>
2025-03-04 20:13:53 +0100

    Calculate remaining attempts in useHangmanGame hook

--- src/hooks/useHangmanGame.js
+++ src/hooks/useHangmanGame.js
@@ -9,4 +9,6 @@ export const useHangmanGame = () => {
   const incorrectLetters = guessedLetters.filter(
     (letter) => !word.includes(letter)
   )
+
+  const attemptsRemaining = maxAttempts - incorrectLetters.length
 }
