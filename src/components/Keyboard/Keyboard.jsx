e9b995be337c48534b314f920e5468c231f9186c
Espadv69 <aitorjob69@gmail.com>
2025-03-04 17:22:01 +0100

    Enhance Keyboard component to handle guessed letters and letter click events

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,3 +1,19 @@
-const KeyBoard = () => {}
+const KeyBoard = ({ guessedLetters, onLetterClick }) => {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+
+  return (
+    <div className="keyboard">
+      {alphabet.split('').map((letter) => (
+        <button
+          key={letter}
+          onClick={() => onLetterClick(letter)}
+          disabled={guessedLetters.includes(letter)}
+        >
+          {letter}
+        </button>
+      ))}
+    </div>
+  )
+}
 
 export default KeyBoard
