9b5b1e450b24c60c222f22af967d8bc386fc8036
Espadv69 <aitorjob69@gmail.com>
2025-03-05 17:44:48 +0100

    Refactor word list to categorize words by type

--- src/utils/words.js
+++ src/utils/words.js
@@ -1,15 +1,15 @@
-const WORDS = [
-  'REACT',
-  'VITE',
-  'JAVASCRIPT',
-  'DEVELOPER',
-  'HANGMAN',
-  'COMPONENT',
-  'HOOK',
-  'STATE',
-  'PROPS',
-  'RENDER',
-]
+const wordsByCategory = {
+  animals: [
+    'ELEPHANT',
+    'GIRAFFE',
+    'KANGAROO',
+    'PENGUIN',
+    'DOLPHIN',
+    'CHEETAH',
+    'FLAMINGO',
+    'RACCOON',
+  ],
+}
 
 export const getRandomWord = () => {
   return WORDS[Math.floor(Math.random() * WORDS.length)]
